32493c80eca642aaf1b3eee916dc5e4ed177a6b6
oğuz alp çepni <oguzalpcepni@gmail.com>
2024-08-16 12:14:58 +0300

    Initial commit

---
+++ src/main/java/basicauth/springsecuriytokenbased/SpringSecuriyTokenbasedApplication.java
@@ -0,0 +1,13 @@
+package basicauth.springsecuriytokenbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuriyTokenbasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuriyTokenbasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/basicauth/springsecuriytokenbased/SpringSecuriyTokenbasedApplicationTests.java
@@ -0,0 +1,13 @@
+package basicauth.springsecuriytokenbased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuriyTokenbasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
